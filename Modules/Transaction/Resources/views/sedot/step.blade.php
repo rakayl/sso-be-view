df527d68ee5410363156f534d45b1b59393ae66c
rakayl <rakayl691@gmail.com>
2023-12-21 05:03:54 +0700

    dev

--- Modules/Transaction/Resources/views/sedot/step.blade.php
+++ Modules/Transaction/Resources/views/sedot/step.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-3">
                 <ul class="ver-inline-menu tabbable margin-bottom-10">
-                        <li @if($detail['transaction_status_code']==3||$detail['step_number']==7) class="active" @endif>
+                        <li @if($detail['transaction_status_code']!=4) class="active" @endif>
                                 <a @if($detail['transaction_status_code']==3||in_array($detail['step_number'], [1,2,3,4,5,6,7])) data-toggle="tab" href="#step1" @else style="opacity: 0.5 !important;pointer-events: none;" @endif><i class="fa fa-cog"></i> Pemilihan Vendor </a>
                         </li>
                         <li @if($detail['transaction_status_code']!=3&&$detail['step_number']==1) class="active" @endif>
@@ -23,14 +23,12 @@
                         <li @if($detail['transaction_status_code']!=3&&$detail['step_number']==5) class="active" @endif>
                                 <a  @if($detail['transaction_status_code']!=3&&in_array($detail['step_number'], [5,6,7])) data-toggle="tab" href="#step6" @else style="opacity: 0.5 !important;pointer-events: none;" @endif><i class="fa fa-cog"></i> Pembayaran</a>
                         </li>
-                        <li @if($detail['transaction_status_code']!=3&&$detail['step_number']==6) class="active" @endif>
-                                <a  @if($detail['transaction_status_code']!=3&&in_array($detail['step_number'], [6,7])) data-toggle="tab" href="#step7" @else style="opacity: 0.5 !important;pointer-events: none;" @endif><i class="fa fa-cog"></i> Verifikasi Pembuangan Limbah</a>
-                        </li>
+                      
                 </ul>
         </div>
         <div class="col-md-9">
                 <div class="tab-content">
-                        <div class="tab-pane @if($detail['transaction_status_code']==3||$detail['step_number']==6) active @endif" id="step1">
+                        <div class="tab-pane @if($detail['transaction_status_code']!=4) active @endif" id="step1">
                                 @include('transaction::sedot.steps.step1')
                         </div>
                         <div class="tab-pane @if($detail['transaction_status_code']!=3&&$detail['step_number']==1) active @endif" id="step2">
@@ -47,9 +45,6 @@
                         </div>
                         <div class="tab-pane @if($detail['step_number']==5) active @endif" id="step6">
                                 @include('transaction::sedot.steps.step6')
-                        </div>
-                        <div class="tab-pane @if($detail['step_number']==6) active @endif" id="step7">
-                                @include('transaction::sedot.steps.step7')
                         </div>
                 </div>
         </div>
